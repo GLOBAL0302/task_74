83646a05b486bffdd217c38263df496f47b90772
Abdashimov Kubanychbek <abdashimov99@mail.ru>
2024-08-06 02:08:18 +0400

    added routing

---
+++ routers/message.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import {promises as fs} from "fs";
+import {IMessages} from "../types";
+
+const messageRouter = express.Router();
+const path = "message"
+
+
+messageRouter.get("/", async(req, res) => {
+    const allMessages:IMessages[] = []
+    const files = await fs.readdir("message")
+    for (const file of files) {
+        const fileContent = await fs.readFile(path + "/" + file)
+        const result = JSON.parse(fileContent.toString())
+        allMessages.push(result)
+    }
+    res.send(allMessages);
+});
+
+messageRouter.post("/", async(req, res)=>{
+    const message:IMessages = {
+        message: req.body.message,
+        date: new Date().toISOString(),
+    }
+    await fs.writeFile(`${path}/${message.date}.txt`, JSON.stringify(message));
+    res.send(message)
+})
+
+export default messageRouter;
